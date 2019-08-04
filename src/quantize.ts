2f24854abe9aa26987f040aa97f98f05b2a4e3a0
helloheesu <heesu.jung.developer@gmail.com>
2019-08-05 03:09:56 +0900

    :fire: pv - useless params, methods

--- src/quantize.ts
+++ src/quantize.ts
@@ -12,34 +12,16 @@
  */
 if (!pv) {
   var pv = {
-    map: function(array, f) {
-      const o = {};
-      return f
-        ? array.map(function(d, i) {
-            o.index = i;
-            return f.call(o, d);
-          })
-        : array.slice();
-    },
-    naturalOrder: function(a, b) {
+    naturalOrder: function(a: number, b: number) {
       return a < b ? -1 : a > b ? 1 : 0;
     },
-    sum: function(array, f) {
-      const o = {};
-      return array.reduce(
-        f
-          ? function(p, d, i) {
-              o.index = i;
-              return p + f.call(o, d);
-            }
-          : function(p, d) {
-              return p + d;
-            },
-        0
-      );
+    sum: function(array: number[]) {
+      return array.reduce(function(p, d) {
+        return p + d;
+      }, 0);
     },
-    max: function(array, f) {
-      return Math.max.apply(null, f ? pv.map(array, f) : array);
+    max: function(array: number[]) {
+      return Math.max.apply(null, array);
     }
   };
 }
