ea28ff66f81a815d452683e38a36f69b131b3f7b
Lokesh Dhakar <lokesh.dhakar@gmail.com>
2024-10-03 13:27:47 -0700

    test: add jest and simple tests

---
+++ src/__tests__/quantize.test.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const quantize = require(path.join(__dirname, '..', '..', 'dist', 'index.umd.js'));
+
+
+describe("Basic usage from README.md", () => {
+  const maximumColorCount = 4;
+  let arrayOfPixels;
+  let colorMap;
+
+  beforeEach(() => {
+    arrayOfPixels = [
+      [190, 197, 190],
+      [202, 204, 200],
+      [207, 214, 210],
+      [211, 214, 211],
+      [205, 207, 207]
+    ];
+    colorMap = quantize(arrayOfPixels, maximumColorCount);
+  });
+
+  it("Reduced Palette", () => {
+    expect(colorMap.palette()).toStrictEqual([
+      [204, 204, 204],
+      [208, 212, 212],
+      [188, 196, 188],
+      [212, 204, 196]
+    ]);
+  });
+
+  it("Reduced pixel", () => {
+    expect(colorMap.map(arrayOfPixels[0])).toStrictEqual([188, 196, 188]);
+  });
+});
